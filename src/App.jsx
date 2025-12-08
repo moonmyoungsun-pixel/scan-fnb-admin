ceef583549b648fd85b34dbd1ce6bcbeb6e9be27
Moon Myoungsun <anyevery@naver.com>
2025-12-09 02:41:52 +0900

    Initial commit - React admin setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold text-red-500">
+        Hello Tailwind!
+      </h1>
+    </div>
+  )
+}
